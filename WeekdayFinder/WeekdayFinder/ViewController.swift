3df90efcf64f93eecbc7a0e94b4033e1c866e648
gulivero1773 <61082418+gulivero1773@users.noreply.github.com>
2020-02-29 13:23:33 +0200

    commit fit Weekday

--- WeekdayFinder/WeekdayFinder/ViewController.swift
+++ WeekdayFinder/WeekdayFinder/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findWeekDay () {
+        
+        let calendar = Calendar.current
+        
+        var dateComponents = DateComponents()
+        if let day = dateTF.text, let month = monthTF.text, let year = yearTF.text{
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+            guard let date = calendar.date(from: dateComponents) else {return}
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        let weekday = dateFormatter.string(from: date)
+        
+        resultLabel.text = weekday
+        
     }
-
-
+    
+        func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
+}
